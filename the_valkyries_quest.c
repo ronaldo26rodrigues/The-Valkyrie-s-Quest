0606a121570dd37d39e7241411c8cb083fa759ae
Ronaldo <ronaldo1420155@gmail.com>
2021-01-25 22:17:08 -0300

    first commit

---
+++ the_valkyries_quest.c
@@ -0,0 +1,72 @@
+/*=========================================================================*
+ *  The Valkyrie's Quest
+ *  
+ *  Desenvolvido por: Esdras, Gabriel, Gustavo, Marissol, Ronaldo e Vicente
+ *=========================================================================*/
+
+#include <raylib.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+
+//========
+// defines
+
+
+//----------
+
+
+//===========
+// estruturas
+
+
+//-------------
+
+
+//=================
+// Variaveis gloais
+
+static const int screenWidth = 1000;
+static const int screenHeight = 650;
+
+//-----------------
+
+
+//========
+// Funções
+
+
+//--------
+
+
+
+int main(){
+    srand(time(NULL));
+    
+    InitWindow(screenWidth, screenHeight, "The Valkyrie's Quest");
+    SetTargetFPS(60);
+    
+    
+    
+    while(!WindowShouldClose()){
+        
+        
+        
+        BeginDrawing();
+        
+        ClearBackground(BLACK);
+        
+        
+        EndDrawing();
+        
+    }
+    
+    CloseWindow();
+    
+}
