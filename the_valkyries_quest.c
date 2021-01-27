1f36b78baef9d0170f0d2d7ed381a88225e3d3dc
Ronaldo <ronaldo1420155@gmail.com>
2021-01-27 13:51:44 -0300

    movimento do personagem e outras configuracoes

--- the_valkyries_quest.c
+++ the_valkyries_quest.c
@@ -25,12 +25,13 @@
 //===========
 // estruturas
 
-typedef struct Valkyrie {
+typedef struct Player {
     Rectangle rec;
     Vector2 speed;
     Color color;
     Vector2 prevPos;
-} Valkyrie;
+    int vida;
+} Player;
 
 //-------------
 
@@ -41,6 +42,11 @@ typedef struct Valkyrie {
 static const int screenWidth = 1000;
 static const int screenHeight = 650;
 
+static bool gameOver = false;
+static bool pause =  false;
+static bool victory = false;
+
+static Player player;
 //-----------------
 
 
@@ -49,9 +55,16 @@ static const int screenHeight = 650;
 
 static void initGame(void);
 static void movement(void);
+void delay(float seconds);
 
 //--------
 
+//====================
+// carregando texturas
+
+
+
+//--------------------
 
 
 int main(){
@@ -60,6 +73,7 @@ int main(){
     InitWindow(screenWidth, screenHeight, "The Valkyrie's Quest");
     SetTargetFPS(60);
     
+    initGame();
     
     
     while(!WindowShouldClose()){
@@ -69,19 +83,52 @@ int main(){
         BeginDrawing();
         ClearBackground(BLACK);
         
+        DrawRectangleRec(player.rec, player.color);
+        
+        movement();
+        
         
         EndDrawing();
         
     }
     
-    CloseWindow();
-    
+    CloseWindow(); 
 }
 
 void initGame(){
     // inicia os paramentros do jogo
+    
+    // jogador
+    player.rec.x =  screenWidth/2;
+    player.rec.y = screenHeight/2;
+    player.rec.width = 25;
+    player.rec.height = 25;
+    player.speed.x = 5;
+    player.speed.y = 5;
+    player.color = YELLOW;
+    
 }
 
 void movement(){
     // movimento do personagem
+    
+    if(IsKeyDown(KEY_RIGHT) && player.rec.x<screenWidth-40) {
+        player.rec.x += player.speed.x;
+    }
+    if(IsKeyDown(KEY_LEFT) && player.rec.x>30){
+        player.rec.x -= player.speed.x;
+    }
+    if(IsKeyDown(KEY_DOWN) && player.rec.y<screenHeight-25){
+        player.rec.y += player.speed.y;
+    }
+    if(IsKeyDown(KEY_UP) && player.rec.y>20){
+        player.rec.y -= player.speed.y;
+    }
+}
+
+void delay(float seconds){
+    float milliseconds = seconds*1000;
+    clock_t start = clock();
+
+    while((clock() - start) * 1000 / CLOCKS_PER_SEC < milliseconds);
 }
